084e8897b3f13d3d88589c0a0c4433e11bd6be5c
vanbujhm <vanbujhm@gmail.com>
2016-06-21 09:08:52 +0300

    Revert "Revert "Create project v0.1""
    This reverts commit a191a90d4549b561943b0164f193f2ad455c4b14.

---
+++ bitrix/admin/custom_redirect_index.php
@@ -0,0 +1,1 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/local/modules/custom_redirect/admin/custom_redirect_index.php");?>
---
+++ local/modules/custom_redirect/lang/ru/admin/menu.php
@@ -0,0 +1,5 @@
+<?php
+defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
+
+$MESS['module_name'] = 'Редиректы';
+$MESS['module_tab_list'] = 'Список редиректов';
